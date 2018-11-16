9797f65f36273b594693d9c7f9f2fae5656677df
mohammad saleh <salehos763@gmail.com>
2018-11-16 13:56:56 +0330

    Sort kardane list

--- Hello.py
+++ Hello.py
@@ -5,6 +5,11 @@ float_one = 1.0
 float_one = float_one + one
 print(type(float_one))
 list_1 = [1 , 2]
-list_2 = [ 4 , 3 ]
+list_2 = [ 3 , 0 ]
 
 print(list_1 + list_2)
+
+list_sorted = list_1+list_2
+
+print(sorted(list_sorted))
+#sort kardane list
