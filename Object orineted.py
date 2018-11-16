b02a7e03f7c6793b9dc07d14ff6553edb8630330
mohammad saleh <salehos763@gmail.com>
2018-11-16 15:48:48 +0330

    property

--- Object orineted.py
+++ Object orineted.py
@@ -5,22 +5,22 @@ class Animal:
         self._name = name
         self._age = age
 
+    @property
     def get_name(self):
         return "my name is %s and my age is %d" % (self._name, self._age)
+    #property baes mishe tabe faqat returnesh kar kone va baghie dastresi behesh be soorate kamel nadashte bashan va be onvane tabe ba () nmiad
 
 class Cat(Animal):
-    def __init__(self , age):
-        super(Cat, self).__init__("cat" , age)
+    def __init__(self, age):
+        super(Cat, self).__init__("cat", age)
+
 
 
 dog = Animal("dog", 10)
 cat = Cat(15)
 
-
-
-
-print(dog.get_name())
-print(cat.get_name())
+print(dog.get_name)
+print(cat.get_name)
 print(type(dog))
 print(cat.__class__)
 print("\n")
