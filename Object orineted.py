6e190930275116cdf60977d1a73a033cdbb793c4
mohammad saleh <salehos763@gmail.com>
2018-11-16 15:42:20 +0330

    inheritance va zir class o bache o ayal o in jor chiza :|

--- Object orineted.py
+++ Object orineted.py
@@ -1,4 +1,6 @@
 class Animal:
+    type = "Animal"
+
     def __init__(self, name, age):
         self._name = name
         self._age = age
@@ -6,7 +8,20 @@ class Animal:
     def get_name(self):
         return "my name is %s and my age is %d" % (self._name, self._age)
 
-dog = Animal("dog" , 10)
-cat = Animal("cat" , 15)
+class Cat(Animal):
+    def __index__(self , age):
+        super(Cat, self).__init__("cat" , age)
+
+
+dog = Animal("dog", 10)
+cat = Cat(15)
+
+
+
+
+
 print(dog.get_name())
 print(cat.get_name())
+print(type(dog))
+print(cat.__class__)
+print("\n")
